a528b29c4cacf4be42a02c4ec82bfbde86286eee
thanaa <thanaamater@gmail.com>
2024-12-15 19:19:08 +0200

    update file

--- ConsoleApp1/obj/Debug/net8.0-windows/ConsoleApp1.AssemblyInfo.fs
+++ ConsoleApp1/obj/Debug/net8.0-windows/ConsoleApp1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbdc3687908991d29754e49784fe8a5f43f46fd9")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
